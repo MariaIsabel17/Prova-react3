4741ff2065889577688df6027ab74a99104cacc0
Victória Glória <40142330+victoria2309@users.noreply.github.com>
2019-08-20 16:17:23 -0300

    base-js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import api from './services/api';
+import './styles.css';
+import Header from './components/Header';
+import Main from './pages/main'; 
+const App = () => (
+  <div className="App">
+  <Header />
+  <Main />
+  </div>
+);
+
+
+export default App;
